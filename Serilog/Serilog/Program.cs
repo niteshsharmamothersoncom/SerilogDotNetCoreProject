0d36c163ba4e14029f6eab0b5de24220f4fcfdc7
niteshsharmamothersoncom <nitesh.sharma@motherson.com>
2023-08-14 16:12:44 +0530

    swagger api

--- Serilog/Serilog/Program.cs
+++ Serilog/Serilog/Program.cs
@@ -8,38 +8,47 @@ using NLog;
 
 
 var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
-try 
+try
 
-{ 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-builder.Host.UseSerilog((context, configuration) => configuration.ReadFrom.Configuration(context.Configuration));
-
-builder.Host.UseNLog();
-
-
-
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-}
-app.UseStaticFiles();
+    var builder = WebApplication.CreateBuilder(args);
 
-app.UseRouting();
-app.UseSerilogRequestLogging();
-app.UseAuthorization();
+    // Add services to the container.
+    builder.Services.AddControllersWithViews();
+    builder.Host.UseSerilog((context, configuration) => configuration.ReadFrom.Configuration(context.Configuration));
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    builder.Host.UseNLog();
 
-app.Run();
+
+
+
+    var services = builder.Services;
+    services.AddEndpointsApiExplorer();
+    services.AddSwaggerGen();
+    var app = builder.Build();
+
+
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Home/Error");
+    }
+    app.UseStaticFiles();
+
+    app.UseRouting();
+    app.UseSerilogRequestLogging();
+    app.UseAuthorization();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    });
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+
+    app.Run();
 
 }
 catch (Exception exception)
